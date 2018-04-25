7b47929899b1d53c0e447fbd372ac1f8a6eec8fa
Uni <1025687605@qq.com>
2018-04-25 21:46:37 +0800

    添加菜单关闭功能

--- Timo-React/src/pages/layout/nav/dropdown.jsx
+++ Timo-React/src/pages/layout/nav/dropdown.jsx
@@ -4,21 +4,25 @@ import TimoList from './list'
 class DropDown extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isShow: false
+        }
         this.handleClick = this.handleClick.bind(this);
     }
     handleClick() {
-
+        this.setState({isShow: !this.state.isShow})
     }
     render() {
         let item = this.props.item;
         return (
             <div>
-                <div className="main-menu">
+                <div className="main-menu" onClick={this.handleClick}>
                     <img src={item.imgPath} alt="blink" className="img"/>
                     <span>{item.name}</span>
                     <span className="icon-great">&gt;</span>
                 </div>
-                <TimoList childLists = {item.children} path={this.props.path}/>
+                {this.state.isShow ? 
+                <TimoList childLists = {item.children} path={this.props.path}/> : null}    
             </div>
         )
     }
