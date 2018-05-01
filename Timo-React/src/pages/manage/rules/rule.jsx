1edb0b17622ec332c87a358f510258f30601d56c
Uni <1025687605@qq.com>
2018-05-01 19:13:02 +0800

    新增react-redux相关文件

--- Timo-React/src/pages/manage/rules/rule.jsx
+++ Timo-React/src/pages/manage/rules/rule.jsx
@@ -7,9 +7,6 @@ class Rule extends Component {
     constructor(props) {
         super(props);
     }
-    componentWillMount() {
-        
-    }
     render() {
         return(
             <div className="rule">
