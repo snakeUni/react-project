fb919732433297b2abf4630094f388b008e834e9
Uni <1025687605@qq.com>
2018-05-02 21:00:08 +0800

    获取所有记录api

--- Timo-React/src/pages/manage/record/index.jsx
+++ Timo-React/src/pages/manage/record/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import TimoTable from '../../../components/table/index'
 import TimoPagination from '../../../components/pagination/index'
+import TimoApi from '../../../api/api'
 import './index.css'
 
 class Record extends Component {
@@ -8,51 +9,53 @@ class Record extends Component {
         super(props);
         this.state = {
             columns: [{
-                title: '姓名',
-                dataIndex: 'name',
-                key: 'name'
+                title: '不生气',
+                dataIndex: 'happy',
+                key: 'happy'
             }, {
-                title: '年龄',
-                dataIndex: 'age',
-                key: 'age'
+                title: '准时睡觉',
+                dataIndex: 'sleep',
+                key: 'sleep'
             }, {
-                title: '地址',
-                dataIndex: 'address',
-                key: 'address'
+                title: '准时学习',
+                dataIndex: 'study',
+                key: 'study'
+            }, {
+                title: '初始星星',
+                dataIndex: 'primary',
+                key: 'primary'
+            }, {
+                title: '额外奖励',
+                dataIndex: 'extra',
+                key: 'extra'
+            }, {
+                title: '其他奖励',
+                dataIndex: 'other',
+                key: 'other'
             }],
-            dataSource: [{
-                key: '1',
-                name: '胡彦斌',
-                age: 32,
-                address: '西湖区湖底公园1号'
-              }, {
-                key: '2',
-                name: '胡彦祖',
-                age: 42,
-                address: '西湖区湖底公园1号'
-              }, {
-                key: '3',
-                age: 52,
-                name: '胡彦祖',
-                address: '西湖区湖底公园1号'
-              }, {
-                key: '4',
-                name: '胡彦斌',
-                age: 32,
-                address: '西湖区湖底公园1号'
-              }, {
-                key: '5',
-                name: '胡彦祖',
-                age: 42,
-                address: '西湖区湖底公园1号'
-              }, {
-                key: '6',
-                age: 52,
-                name: '胡彦祖',
-                address: '西湖区湖底公园1号'
-              }]
+            dataSource: []
         }
         this.handleChange = this.handleChange.bind(this);
+    }
+    componentWillMount() {
+        let result = TimoApi.getDayRecord({params: {userId: 1}});
+        let dataSource = [];
+        result.then((data) => {
+            let response = data.get_response.result;
+            dataSource = response.map((item) => {
+                return { 
+                    key: item.id,
+                    happy: item.happy,
+                    sleep: item.sleep,
+                    study: item.study,
+                    primary: item.primaryStar,
+                    extra: item.rewardOther,
+                    other: item.other
+                }                
+            })
+            this.setState({dataSource: dataSource});
+        })
+        
     }
     handleChange(currentpage) {
     }
