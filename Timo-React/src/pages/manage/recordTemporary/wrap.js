c2d0afb163b6b1beb3c9efc8a9b55e906ea1fb2b
Uni <1025687605@qq.com>
2018-05-09 19:02:53 +0800

    增加了高阶组件以及loading

---
+++ Timo-React/src/pages/manage/recordTemporary/wrap.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import TimoApi from '../../../api/api'
+
+/**
+ * @param {WrappedComponent} 包裹组件
+ * @param {data} 传递的数据
+ * @returns {NewWrapComponent} 新的组件
+ */
+export default (WrappedComponent, data) => {
+    class NewWrapComponent extends Component {
+        constructor () {
+            super()
+            this.state = {
+                dataSource: []
+            }
+        }
+        componentWillMount () {
+            // 做一些请求
+        }
+        render () {
+            return <WrappedComponent dataSource={this.state.dataSource} {...this.props}/>
+        }   
+    }
+    return NewWrapComponent
+}
