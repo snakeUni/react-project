6cac562db84f99108fbc0ccabad34138b7960243
Uni <1025687605@qq.com>
2018-05-12 22:43:19 +0800

    添加了actiontypes

---
+++ Timo-React/src/store/action-type.js
@@ -0,0 +1,30 @@
+/**
+ * 获取记录
+ */
+const GET_RECORD = 'GET_RECORD'
+
+/**
+ * 增加记录
+ */
+const ADD_RECORD = 'ADD_RECORD'
+
+/**
+ * 获取奖项列表
+ */
+const GET_REWARDS = 'GET_REWARDS'
+
+/**
+ * 获取用户信息
+ */
+const GET_USERINFO = 'GET_USERINFO'
+
+/**
+ * 获取频道信息
+ */
+const GET_CHANNEL = 'GET_CHANNEL'
+
+/**
+ * 用户注册
+ */
+const REGISTER_USER = 'REGISTER_USER'
+
